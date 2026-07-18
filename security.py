6e1d364614111ef58e888942085ec4cc3005967a
iti455 <itismitadas2004@gmail.com>
2026-07-18 15:58:32 +0530

    Delete security.py

--- security.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hash_secret(secret: str) -> str:
-    return pwd_context.hash(secret)
-
-def verify_secret(secret: str, hashed: str) -> bool:
-    return pwd_context.verify(secret, hashed)
